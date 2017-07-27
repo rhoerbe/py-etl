5c75c48be7e5ae6413df8835fcb1dc601d71b43b
Ralf Schlatterbeck <rsc@runtux.com>
2017-07-27 20:41:38 +0200

    Add update for test-data and eventlog

--- testdriver.py
+++ testdriver.py
@@ -72,7 +72,7 @@ class ODBC_Connector (object) :
         self.cursor = self.cnx.cursor ()
     # end def __init__
 
-    # function starting with cmd_ implement external commands
+    # functions starting with cmd_ implement external commands
     # to be called via command-line interface
 
     def cmd_initial_load (self) :
@@ -80,6 +80,12 @@ class ODBC_Connector (object) :
         self.create_tables ()
         self.load_initial_testdata ()
     # end def cmd_initial_load
+
+    def cmd_update (self, update_number) :
+        self.update_data ('testdata/changeset%s.csv' % update_number)
+        self.load_data \
+            ('testdata/eventlog%s.csv' % update_number, 'eventlog_ph')
+    # end def cmd_update
 
     def create_tables (self) :
         """ Create the tables in the given database
@@ -102,51 +108,95 @@ class ODBC_Connector (object) :
     def drop_tables (self) :
         for tbl in self.fields :
             sql = 'drop table %s' % tbl
-            try :
-                self.cursor.execute (sql)
-            except pyodbc.Error as cause :
-                if 'already exists' not in str (cause) :
-                    raise
+            self.cursor.execute (sql)
     # end def drop_tables
+
+    def insert (self, table, d) :
+        fn  = sorted (d.keys ())
+        sql = 'insert into %s (%s) values (%s)'
+        sql = \
+            ( sql
+            % ( table
+              , ','.join (k for k in fn if d [k])
+              , ','.join (self.to_sql_tpl (k) for k in fn if d [k])
+              )
+            )
+        v   = (self.to_sql (k, d [k]) for k in fn if d [k])
+        self.cursor.execute (sql, *v)
+    # end def insert
+
+    def update (self, table, key, kvalue, d) :
+        fn  = sorted (d.keys ())
+        sql = 'update %s set %s where %s = ?'
+        sql = \
+            ( sql
+            % ( table
+              , ', '.join
+                  ('%s = %s' % (k, self.to_sql_tpl (k, d [k])) for k in fn)
+              , key
+              )
+            )
+        v   = list (self.to_sql (k, d [k]) for k in fn if d [k])
+        #print (sql, v)
+        v.append (kvalue)
+        self.cursor.execute (sql, *v)
+    # end def update
+
+    def load_data (self, filename, table) :
+        with open (filename, 'r', encoding = 'utf-8') as f :
+            dr = DictReader (f, delimiter = ';')
+            for d in dr :
+                self.insert (table, d)
+        self.cursor.commit ()
+    # end def load_data
+
+    def load_initial_testdata (self) :
+        self.load_data ('testdata/initial_data.csv', 'benutzer_alle_dirxml_v')
+    # end def load_initial_testdata
 
     def to_sql (self, k, v) :
         if not v :
             return None
         for f in self.fields :
             if self.fields [f].get (k) == 'double precision' :
-                if not v :
-                    return 0
                 return float (v)
-        if not v :
-            return None
         return v
     # end def to_sql
 
-    def to_sql_tpl (self, k) :
-        if k == 'geburtsdatum' :
-            # Code for oracle
-            #return "to_date (?, 'YYYY-MM-DD HH:MI:SS')"
-            # We're using postgres for testing, so use to_timestamp
-            return "to_timestamp (?, 'YYYY-MM-DD HH24:MI:SS')"
+    def to_sql_tpl (self, k, v = 1) :
+        if not v :
+            return 'NULL'
+        for sk in self.fields :
+            if self.fields [sk].get (k, '').startswith ('timestamp') :
+                # Code for oracle
+                #return "to_date (?, 'YYYY-MM-DD HH:MI:SS')"
+                # We're using postgres for testing, so use to_timestamp
+                # We also need 24 hours clock, otherwise we get an error
+                # message that 0 is not allowed for a 12 hour clock.
+                return "to_timestamp (?, 'YYYY-MM-DD HH24:MI:SS')"
         return '?'
     # end def to_sql_tpl
 
-    def load_initial_testdata (self) :
-        with open ('testdata/initial_data.csv', 'r', encoding = 'utf-8') as f :
+    def update_data (self, filename) :
+        """ Updates to existing records only for benutzer_alle_dirxml_v
+            table, we have a hard-coded pk_uniqueid column in the code
+        """
+        table = 'benutzer_alle_dirxml_v'
+        with open (filename, 'r', encoding = 'utf-8') as f :
             dr = DictReader (f, delimiter = ';')
             for d in dr :
-                fn  = sorted (d.keys ())
-                sql = 'insert into benutzer_alle_dirxml_v (%s) values (%s)'
-                sql = \
-                    ( sql
-                    % ( ','.join (k for k in fn if d [k])
-                      , ','.join (self.to_sql_tpl (k) for k in fn if d [k])
-                      )
-                    )
-                v   = (self.to_sql (k, d [k]) for k in fn if d [k])
-                self.cursor.execute (sql, *v)
+                uid = d ['pk_uniqueid']
+                sql = 'select pk_uniqueid from %s where pk_uniqueid = ?'
+                sql = sql % table
+                self.cursor.execute (sql, float (uid))
+                row = self.cursor.fetchone ()
+                #print ("Got: %s" % row)
+                if row :
+                    self.update (table, 'pk_uniqueid', float (uid), d)
+                else :
+                    self.insert (table, d)
         self.cursor.commit ()
-    # end def load_initial_testdata
+    # end def update_data
 
 # end class ODBC_Connector
 
@@ -161,6 +211,12 @@ def main () :
         , help    = 'Command to execute, one of %s' % ', '.join (commands)
         )
     cmd.add_argument \
+        ( '-A', '--argument'
+        , help    = 'Argument to command'
+        , default = []
+        , action  = 'append'
+        )
+    cmd.add_argument \
         ( '-D', '--database'
         , help    = 'Database to connect to'
         , default = 'postgres'
@@ -168,7 +224,7 @@ def main () :
     args = cmd.parse_args ()
     odbc = ODBC_Connector (args)
     fun  = getattr (odbc, 'cmd_' + args.command)
-    fun ()
+    fun (* args.argument)
 # end def main
 
 if __name__ == '__main__' :
